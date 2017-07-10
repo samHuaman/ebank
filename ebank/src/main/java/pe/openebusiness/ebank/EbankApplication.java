6dc53152cf5c38da67afc021fb9347b63ed22f03
samHuaman <samuel.huaman@openebusiness.pe>
2017-07-10 15:12:53 -0500

    creación del proyecto

---
+++ ebank/src/main/java/pe/openebusiness/ebank/EbankApplication.java
@@ -0,0 +1,12 @@
+package pe.openebusiness.ebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbankApplication.class, args);
+	}
+}
